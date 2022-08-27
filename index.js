48e20bd7f256e97f0cb6efa1445e265b89a6795c
zazazaza4 <6715414@gmail.com>
2022-08-27 14:09:34 +0300

    Router

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 import express from 'express';
+import { userRouter } from './users/users.js';
 
 const port = '8000';
 const app = express();
 
 app.get('/hello', (req, res) => {
-  res.send('Hello');
+  res.end();
 });
+
+app.use('/users', userRouter);
 
 app.listen(port, () => {
   console.log(`Server started. http://localhost:${port}`);
