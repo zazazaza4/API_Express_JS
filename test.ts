15f1aad871e463025f5daecc3fda9937bb86178f
zazazaza4 <6715414@gmail.com>
2022-08-27 20:59:00 +0300

    Классы

--- test.ts
+++ test.ts
@@ -171,22 +171,129 @@ function compute({ lat, long }: iCoord) {
 
 // let myDirection = Direction2.Up;
 
-interface HasLength {
-  length: number;
+// interface HasLength {
+//   length: number;
+// }
+
+// function log<T extends HasLength, K>(params: T, arr: K[]): K[] {
+//   console.log(arr.length);
+//   return arr;
+// }
+
+// log<string, number>('sid', [32, 23, 2, 3, 23]);
+// log<HasLength, string>({ length: 21 }, ['ada']);
+
+// interface IUser {
+//   name: string;
+//   age?: number;
+//   bid: <T>(sum: T) => void;
+// }
+
+// function bid<T>(sum: T) {}
+
+class Coord {
+  lat: number;
+  long: number;
+
+  protected test() {
+    if (this.lat > 0) {
+      console.log(this.lat);
+    }
+  }
+
+  computeDistance(newLat: number, newLong: number): number {
+    return 0;
+  }
+
+  constructor(lat: number, long: number) {
+    this.lat = lat;
+    this.long = long;
+  }
 }
 
-function log<T extends HasLength, K>(params: T, arr: K[]): K[] {
-  console.log(arr.length);
-  return arr;
+const point = new Coord(12, 123);
+
+class MapLocation extends Coord {
+  private _name: string;
+
+  get name() {
+    return this._name;
+  }
+
+  set name(s: string) {
+    this._name = s[0].toUpperCase() + s.substring(1);
+  }
+
+  override computeDistance(newLat: number, newLong: number): number {
+    console.log(this._name);
+    this.test();
+    return 1;
+  }
+
+  constructor(lat: number, long: number, name: string) {
+    super(lat, long);
+    this._name = name;
+  }
 }
 
-log<string, number>('sid', [32, 23, 2, 3, 23]);
-log<HasLength, string>({ length: 21 }, ['ada']);
+const loc = new MapLocation(1, 3, 'lac');
 
-interface IUser {
+interface LoggerService {
+  log: (s: string) => void;
+}
+
+class Logger implements LoggerService {
+  public log(s: string) {
+    console.log(s);
+  }
+
+  private error() {}
+
+  public a = '';
+}
+
+const i = new Logger();
+
+i.log('f');
+
+class MyClass {
+  static a = '1';
+}
+
+MyClass.a;
+
+class MyClassTwo<T> {
+  a: T;
+}
+
+const b = new MyClassTwo<string>();
+b.a;
+
+abstract class Base {
+  print(s: string) {
+    console.log(s);
+  }
+
+  abstract error(s: string): void;
+}
+
+class BaseExtended extends Base {
+  error(s: string) {
+    console.log('Error' + s);
+  }
+}
+
+const base = new BaseExtended();
+
+base.print('Base');
+
+class Animal {
   name: string;
-  age?: number;
-  bid: <T>(sum: T) => void;
 }
 
-function bid<T>(sum: T) {}
+class Dog {
+  name: string;
+  tail: boolean;
+}
+
+const puppy: Animal = new Dog();
