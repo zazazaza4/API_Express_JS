0e696ef2dc4d2e5b1cad2806872bc83bedb426ca
zazazaza4 <6715414@gmail.com>
2022-08-27 14:51:00 +0300

    Промежуточные обработчики

--- index.js
+++ index.js
@@ -4,11 +4,21 @@ import { userRouter } from './users/users.js';
 const port = '8000';
 const app = express();
 
+app.use((req, res, next) => {
+  console.log('Time', Date.now());
+  next();
+});
+
 app.get('/hello', (req, res) => {
-  res.end();
+  throw new Error('Error');
 });
 
 app.use('/users', userRouter);
+
+app.use((err, req, res, next) => {
+  console.log(err.message);
+  res.status(500).send(err.message);
+});
 
 app.listen(port, () => {
   console.log(`Server started. http://localhost:${port}`);
--- users/users.js
+++ users/users.js
@@ -1,6 +1,12 @@
 import express from 'express';
+import { nextTick } from 'process';
 
 const userRouter = express.Router();
+
+userRouter.use((req, res, next) => {
+  console.log('Boom');
+  next();
+});
 
 userRouter.post('/login', (req, res) => {
   res.send('login');
